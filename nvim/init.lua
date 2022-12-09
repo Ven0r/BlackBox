9fa4e5cb76ccb5276f461a9d4a91bb534472971f
Venor <venatorl4w@gmail.com>
2022-12-09 07:16:40 +0000

    added nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,7 @@
+require('settings')
+require('packer-config')
+require('bufferline-config')
+require('colorschemes-config.nightfox')
+require('nvim-tree-config')
+require('mappings')
+require('cmp-config')
