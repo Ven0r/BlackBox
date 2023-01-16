839a20a098f23d09718e7c943cb0ba46b13da3a0
Venor <venor.stdout@gmail.com>
2023-01-16 17:25:27 -0600

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -7,30 +7,20 @@ sudo apt update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
+sudo apt install glances build-essential python3-setuptools hexedit exif qbittorrent jq cupp -y
+
 cd ~
-wget https://dl.google.com/go/go1.19.4.linux-amd64.tar.gz
-tar -C /usr/local go1.19.4.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-
-echo "-------------------------------------------------------------------"
-
-sudo apt install glances zsh nmap neovim build-essential python3-setuptools hexedit exif qbittorrent snapd openvpn git jq -y
-
-cd
-mkdir tools
-cd tools 
-
-mkdir wordlists
-cd wordlists
+mkdir Tools
 
 echo "-------------------------------------------------------------------"
 echo "------------------ Getting SecLists from Github  ------------------"
 echo "-------------------------------------------------------------------"
 
-wget https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt
 git clone https://github.com/danielmiessler/SecLists.git 
+cd SecLists
+wget https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt
 
-cd ..
+cd ~
 
 echo "-------------------------------------------------------------------"
 echo "---------   Getting PayloadsalltheThings from Github  -------------"
@@ -41,48 +31,14 @@ cd ~
 
 mkdir Targets
 
-wget -q https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-chmod +x install.sh
-./install.sh --unattended
-ZSH=${ZSH:-~/.oh-my-zsh}
-
-#export SHELL="$ZSH"
-# Change default shell to ZSH
-chsh -s /usr/bin/zsh
-#Fix .zsh path, add $HOME/.local/bin to PATH
-sed -i '4iexport PATH=$PATH:$HOME/.local/bin' $HOME/.zshrc
-
-#Change zsh theme
-sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="bira"/g' $HOME/.zshrc
-
-# get .zshrc file
 wget https://raw.githubusercontent.com/Ven0r/OS-setup/master/.zshrc
 
-rm install.sh
-chsh -s /usr/bin/zsh
+git config --global user.email "venor.stdout@gmail.com"
+git config --global user.name "Venor"
 
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
-
-echo -e 'export GOROOT=/usr/local/go' >> $HOME/.zshrc
-echo -e 'export GOPATH=$HOME' >> $HOME/.zshrc
-echo -e 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> $HOME/.zshrc
-echo -e 'export PATH=$PATH:/snap/bin' >> $HOME/.zshrc
-
-echo "-------------------------------------------------------------------"
-echo "------------------- Installing Go Toolz ---------------------------"
-echo "-------------------------------------------------------------------"
-
-source ~/.zshrc
 
 go get -v github.com/projectdiscovery/subfinder/cmd/subfinder
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-go get -v github.com/ffuf/ffuf
 
-sudo mv go /usr/local
-echo "-------------------------------------------------------------------"
-echo "---------- Installed Oh-my-zsh, Next Phase ------------"
-echo "-------------------------------------------------------------------"
 
 ssh-keygen -t rsa -b 4096 -N "" -C "venor@venor.com" -f $HOME/.ssh/venor -q
 #source bash-vim-setup.sh
