f707d3e8da981df3220fffe0e29fa61c74c3c5de
venor <blah@blah.com>
2023-09-11 03:06:04 +0000

    base updated

--- setup.sh
+++ setup.sh
@@ -22,19 +22,20 @@ sudo useradd -m -s /usr/bin/zsh venor && echo "venor:toor" | sudo chpasswd
 sudo usermod -aG sudo venor
 sudo touch /home/venor/.zshrc
 sudo chown venor:venor /home/venor/.zshrc
-sudo su - venor
+sudo su - venor -c "
 
-echo "setting git global for venor"
-git config --global user.email "venor.stdout@gmail.com"
-git config --global user.name "Venor"
+echo 'setting git global for venor'
 
-echo "grab ohmyzsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-echo "change theme in ohmyzsh"
+git config --global user.email 'venor.stdout@gmail.com'
+git config --global user.name 'Venor'
+
+echo 'grab ohmyzsh'
+sh -c '$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)' '' --unattended
+echo 'change theme in ohmyzsh'
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="bira"/' ~/.zshrc
 
-echo "setup ssh key"
-ssh-keygen -t ed25519 -C "venor" -f $HOME/.ssh/venor -q -N ""
+echo 'setup ssh key'
+ssh-keygen -t ed25519 -C 'venor' -f $HOME/.ssh/venor -q -N ''
 
 export GOPATH=$HOME/go
 export GOROOT=/usr/lib/go
@@ -44,18 +45,18 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim
 
 cd ~
 
-echo "-------------------------------------------------------------------"
-echo "------------------ Getting SecLists from Github  ------------------"
-echo "-------------------------------------------------------------------"
+echo '-------------------------------------------------------------------'
+echo '------------------ Getting SecLists from Github  ------------------'
+echo '-------------------------------------------------------------------'
 
 git clone https://github.com/danielmiessler/SecLists.git
 cd SecLists
 wget https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt
 cd ~
 
-echo "-------------------------------------------------------------------"
-echo "---------   Getting PayloadsalltheThings from Github  -------------"
-echo "-------------------------------------------------------------------"
+echo '-------------------------------------------------------------------'
+echo '---------   Getting PayloadsalltheThings from Github  -------------'
+echo '-------------------------------------------------------------------'
 
 git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 
@@ -63,15 +64,15 @@ mkdir Targets
 
 wget https://raw.githubusercontent.com/Ven0r/OS-setup/master/.zshrc
 
-echo "Cleaning Up" &&
+echo 'Cleaning Up' &&
 	apt -f install &&
 	apt -y autoremove &&
 	apt -y autoclean &&
 	apt -y clean
 
-echo "-------------------------------------------------------------------"
-echo "-------------------- System Clean, Rebooting ----------------------"
-echo "-------------------------------------------------------------------"
+echo '-------------------------------------------------------------------'
+echo '-------------------- System Clean, Rebooting ----------------------'
+echo '-------------------------------------------------------------------'
 
 sudo reboot
-
+"
