37916c6bf070ab5830ef61c475a07f42b9b83f23
venor <blah@blah.com>
2023-09-11 01:46:30 +0000

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,18 +7,14 @@ sudo apt update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
-sudo apt install glances fuse build-essential python3-setuptools hexedit exif qbittorrent jq cupp -y
+sudo apt install glances fuse build-essential python3-setuptools hexedit golang exif qbittorrent jq cupp -y
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-./nvim.appimage
-
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 
-cd ~
-mkdir Tools
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
 cd ~
 
 echo "-------------------------------------------------------------------"
@@ -28,7 +24,6 @@ echo "-------------------------------------------------------------------"
 git clone https://github.com/danielmiessler/SecLists.git
 cd SecLists
 wget https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt
-
 cd ~
 
 echo "-------------------------------------------------------------------"
@@ -36,23 +31,31 @@ echo "---------   Getting PayloadsalltheThings from Github  -------------"
 echo "-------------------------------------------------------------------"
 git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 
-cd ~
-
 mkdir Targets
 
 wget https://raw.githubusercontent.com/Ven0r/OS-setup/master/.zshrc
 
-cd
+echo "-------------------------------------------------------------------"
+echo "---------               Create User                   -------------"
+echo "-------------------------------------------------------------------"
+sudo adduser --shell /usr/bin/zsh venor
+echo "toor" | sudo chpasswd
+sudo usermod -aG sudo venor
 
 git config --global user.email "venor.stdout@gmail.com"
 git config --global user.name "Venor"
 
-git clone https://github.com/Ven0r/CheatSheets.git
-
-go get -v github.com/projectdiscovery/subfinder/cmd/subfinder
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="bira"/' ~/.zshrc
 
 ssh-keygen -t ed25519 -C "venor" -f $HOME/.ssh/venor -q
-#source bash-vim-setup.sh
+
+git clone https://github.com/Ven0r/CheatSheets.git
+
+export GOPATH=$HOME/go
+export GOROOT=/usr/lib/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
 
 echo "Cleaning Up" &&
 	apt -f install &&
@@ -65,11 +68,4 @@ echo "-------------------- System Clean, Rebooting ----------------------"
 echo "-------------------------------------------------------------------"
 
 sudo reboot
-update os
 
-install zsh
-install ohmyzsh
-add user: "name"
-set user to zshshell
-set user to ohmyzsh
-change zsh to bira theme
